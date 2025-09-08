a1833eff8f447ae9084b12f87b55f1c68b160db2
MayankChandratre1 <mayankmchandratre@gmail.com>
2025-09-08 14:09:00 +0530

    fix: added sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://yourdomain.com",
+  siteUrl: process.env.SITE_URL || "https://www.vxsoftwaresolutions.com",
   generateRobotsTxt: true, // (optional)
   sitemapSize: 7000,       // (optional) split sitemaps if > 7000 URLs
 };
